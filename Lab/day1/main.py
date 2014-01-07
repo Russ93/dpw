c7a9ff154ba82e7469e154e3d96e2e5ffa288047
Russ93 <russ93@fullsail.edu>
2014-01-06 23:09:17 -0500

    fixed the for loop for the grades
    used .append instead of .push
    .append is for python
    .push is for javascript

--- Lab/day1/main.py
+++ Lab/day1/main.py
@@ -4,16 +4,15 @@ DPW
 
 info = dict()
 
-info['name'] = input('Your Name.')
-info['school'] = input('What school have you previously gone to?')
-info['dur'] = input('How many year have you attended this school?')
-info['school'] = input('What school do you go to?')
+#info['name'] = input('Your Name.')
+#info['school'] = input('What school have you previously gone to?')
+#info['dur'] = input('How many year have you attended this school?')
+#info['school'] = input('What school do you go to?')
 info['grades'] = []
 for i in range(0,1000000):
-    grade = input('Write you grades you have and enter # when you are finished, please use only numbers.')
+    grade = input("Write you grades you have and enter '#' with the quoets when you are finished, please use only numbers.")
     if grade == '#':
         break
     else:
-        info['grades'].push(grade)
-        
-'Hi my name is '+info['name']+' and I have gone through '+info['dur']+' year(s) at '+info['school']+'. I have tried to get mostly '+info['grades']+"'s and my GPA is a "info['grades']'. I have tried my hardest in my classes and my grades clearly show it.'
+        info['grades'].append(grade)
+"""'Hi my name is '+info['name']+' and I have gone through '+info['dur']+' year(s) at '+info['school']+'. I have tried to get mostly '+info['grades']+"'s and my GPA is a "info['grades']'. I have tried my hardest in my classes and my grades clearly show it.'"""
