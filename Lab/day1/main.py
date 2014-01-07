03dc9de3bd849b1a851a6e436f3dcae4c6eaf44f
Russ93 <russ93@fullsail.edu>
2014-01-06 22:40:09 -0500

    Wrote the story

--- Lab/day1/main.py
+++ Lab/day1/main.py
@@ -3,3 +3,5 @@ DPW
 1/6/13'''
 
 info = dict()
+
+'Hi my name is '+info['name']+' and I have gone through '+info['dur']+' year(s) at '+info['school']+'. I have tried to get mostly '+info['grades']+"'s and my GPA is a "info['grades']'. I have tried my hardest in my classes and my grades clearly show it.'
