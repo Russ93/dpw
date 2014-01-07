32aed8878a74074f4811ce5e266e8bd327b10153
Russ93 <russ93@fullsail.edu>
2014-01-06 22:40:30 -0500

    Made the input fields

--- Lab/day1/main.py
+++ Lab/day1/main.py
@@ -4,4 +4,10 @@ DPW
 
 info = dict()
 
+info['name'] = input('Your Name.')
+info['school'] = input('What school have you previously gone to?')
+info['dur'] = input('How many year have you attended this school?')
+info['school'] = input('What school do you go to?')
+info['grades'] = []
+        
 'Hi my name is '+info['name']+' and I have gone through '+info['dur']+' year(s) at '+info['school']+'. I have tried to get mostly '+info['grades']+"'s and my GPA is a "info['grades']'. I have tried my hardest in my classes and my grades clearly show it.'
