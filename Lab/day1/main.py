3dcab65e9c5499a9e95bbf9dca8bab0f7bf3d78e
Russ93 <russ93@fullsail.edu>
2014-01-07 00:23:58 -0500

    A little cleaning up

--- Lab/day1/main.py
+++ Lab/day1/main.py
@@ -1,6 +1,8 @@
-'''Russell Schlup
+'''
+Russell Schlup
 DPW
-1/6/13'''
+1/6/13
+'''
 
 info = dict()
 
@@ -9,20 +11,38 @@ info = dict()
 #info['dur'] = input('How many year have you attended this school?')
 #info['school'] = input('What school do you go to?')
 info['grades'] = []
+
+# The asking of what your grades are
 for i in range(0,1000000):
     if i == 0:
         grade = input("""Write you grades you have and enter '#' with the quoets when you are finished,
 please use only numbers and have them be between 0 and 4.
     """)
     else:
-        grade = input('')
+        grade = input('''   ''')
     if grade == '#':
         break
     else:
         info['grades'].append(grade)
+
+# What your highest grade in classes are
 info['best'] = 0
+
 for num in info['grades']:
     if num > info['best']:
         info['best'] = num
-print info['best']
+
+# Grading system
+if info['best']==4:
+    info['let']='A'
+elif info['best']==3:
+    info['let']='B'
+elif info['best']==2:
+    info['let']='C'
+elif info['best']==1:
+    info['let']='D'
+else:
+    info['let']='F'
+
+# The Story
 """'Hi my name is '+info['name']+' and I have gone through '+info['dur']+' year(s) at '+info['school']+'. I have tried to get mostly '+info['grades']+"'s and my GPA is a "info['grades']'. I have tried my hardest in my classes and my grades clearly show it.'"""
