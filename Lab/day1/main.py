bcf9409996c8ef4580aa2e7c7943f62df2e38fe6
Russ93 <russ93@fullsail.edu>
2014-01-07 22:14:33 -0500

    Made the comments more visible
    - remove the annoying print
    - added other things to help the user swell

--- Lab/day1/main.py
+++ Lab/day1/main.py
@@ -6,20 +6,20 @@ DPW
 
 info = dict()
 
-#info['name'] = input('Your Name.')
-#info['school'] = input('What school have you previously gone to?')
-#info['dur'] = input('How many year have you attended this school?')
-#info['school'] = input('What school do you go to?')
+info['name'] = input('Please print you name in quotes.  ')
+info['school'] = input('What school have you previously gone to? in quotes as well. ')
+info['dur'] = input('How many years have you attended this school? NOT IN QUOTES.   ')
 info['grades'] = []
 
 # The asking of what your grades are
 for i in range(0,1000000):
     if i == 0:
         grade = input("""Write you grades you have and enter '#' with the quoets when you are finished,
-please use only numbers and have them be between 0 and 4.
+please use only numbers and have them be between 0 and 4. NOT IN QUOTES.
     """)
     else:
-        grade = input('''   ''')
+        grade = input('''    ''')
+#-------------------- Checker --------------------#
     if grade == '#':
         break
     else:
@@ -32,7 +32,7 @@ for num in info['grades']:
     if num > info['best']:
         info['best'] = num
 
-# Grading system
+#-------------------- Grading system --------------------#
 if info['best']==4:
     info['let']='A'
 elif info['best']==3:
@@ -44,7 +44,7 @@ elif info['best']==1:
 else:
     info['let']='F'
     
-#Adding up The GPA
+#-------------------- Adding up The GPA --------------------#
 info['gpa'] = 0
 
 amt = 0
@@ -55,8 +55,6 @@ for a in info['grades']:
     amt = amt + 1
 
 info['gpa'] = ttl/amt
-
-print info['gpa']
 
 # The Story
 #Hi my name is {info['name']}+ and I have gone through {info['dur']} year(s) at {info['school']}. I have tried to get mostly {info['let']}'s and my GPA is a {info['gpa']}. I have tried my hardest in my classes and my grades clearly show it.
