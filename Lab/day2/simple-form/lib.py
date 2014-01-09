cdd1735414e9d81fb226ae57e1b8fc3d11743811
Russ93 <russ93@fullsail.edu>
2014-01-09 03:06:16 -0500

    Created most of the body elements

--- Lab/day2/simple-form/lib.py
+++ Lab/day2/simple-form/lib.py
@@ -10,6 +10,18 @@ class Page():
 	</head>
 	<body>
 			'''
+		self.__body = '''
+		<nav>
+			<ul>
+				<li><img src="i/l.png" id="logo"/></li>
+				<li>Login</li>
+			</ul>
+		</nav>
+		<section id="cta">
+			<h1>Start Remembering</h1>
+			<p>This app will make modern note taking obsolete and make modern life a bit more manageable by  allowing to take note of everything and never lose information</p>
+			<span>Create a free account</span>
+		'''
 
 		self.__foot= '''
 		</section>
@@ -20,6 +32,10 @@ class Page():
 	def head(self, title):
 		header = self.__header.format(**locals())
 		return header
+
+	def body(self):
+		return self.__body
+
 
 	def foot(self):
 		return self.__foot
