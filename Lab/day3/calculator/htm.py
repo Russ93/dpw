9956229fc9a1c0b18dc18008d1acd95110695653
Russ93 <russ93@fullsail.edu>
2014-01-14 05:15:56 -0500

    Stage 1
    - html and css
    - Region instantiated

---
+++ Lab/day3/calculator/htm.py
@@ -0,0 +1,51 @@
+class Page():
+#-------------------- initializer --------------------#
+	def __init__(self):
+		self.__header = '''
+<!DOCTYPE>
+<html>
+	<head>
+		<title>Countries</title>
+		<link href='http://fonts.googleapis.com/css?family=Lato:300|Josefin+Slab:300' rel='stylesheet' type='text/css'>
+		<link rel='stylesheet' href='css/c.css' />
+	</head>
+	<body>
+		<div id="container">
+			'''
+		self.__form='''
+	 		<form>
+				<input type='text' name='button' value='1' /><button>Asia</button>
+			</form>
+			<form>
+				<input type='text' name='button' value='2' /><button>Africa</button>
+			</form>
+			<form>
+				<input type="text" name="button" value="3" /><button>Europe</button>
+			</form>
+			<form>
+				<input type="text" name="button" value="4" /><button>Latin America</button>
+			</form>
+			<form>
+				<input type="text" name="button" value="5" /><button>North America</button>
+			</form>
+			<form>
+				<input type="text" name="button" value="6" /><button>Oceania</button>
+			</form>
+			<form>
+				<input type="text" name="button" value="0" /><button>World</button>
+			</form>
+			'''
+		self.__foot= '''
+		</div>
+	</body>
+</html>
+			'''
+
+	def head(self):
+		return self.__header
+
+	def form(self):
+		return self.__form
+
+	def foot(self):
+		return self.__foot
--- Lab/day3/calculator/main.py
+++ Lab/day3/calculator/main.py
@@ -1,25 +1,31 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Russell Schlup
+# January 13, 2013
+# DPW
 import webapp2
+from htm import Page
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+	def get(self):
+		page = Page()
 
+		self.response.write(page.head())
+
+		self.response.write(page.form())
+		self.response.write(page.foot())
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
+class Region(object):
+	def __init__(self):
+		self.name = ''
+		self.__population = 0
+		self.area = 0
+		self.world = 7162119434
+
+	@property
+	def population(self):
+		return self.__population
+	@population.setter
+	def population(self, value):
+		self.__population = value
+		
