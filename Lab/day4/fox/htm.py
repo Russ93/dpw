91c12fab2d69131037ca121853565f51afc19817
Russ93 <russ93@fullsail.edu>
2014-01-18 19:21:38 -0500

    created html buttons

--- Lab/day4/fox/htm.py
+++ Lab/day4/fox/htm.py
@@ -12,6 +12,16 @@ class Page():
 	</head>
 	<body>
 		<div id="container">'''
+		#-------------------- Buttons --------------------#
+		self.__form='''
+			<nav>
+				<a href='/?animal=0'><button>Herp Seal</button></a>
+				<a href='/?animal=1'><button>Arctic Fox</button></a>
+				<a href='/?animal=2'><button>Arctic Ermine</button></a>
+				<a href='/?animal=3'><button>Arctic Wolf</button></a>
+				<a href='/?animal=4'><button>Arctic Hare</button></a>
+			</nav>'''
+
 		#-------------------- Closers --------------------#
 		self.__foot= '''
 		</div>
