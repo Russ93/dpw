0be47d89b1ac8c45bbb1e314cc8e0db3fdc2a350
Russ93 <russ93@fullsail.edu>
2014-01-18 19:20:29 -0500

    beginner html elements

---
+++ Lab/day4/fox/htm.py
@@ -0,0 +1,14 @@
+class Page():
+#-------------------- initializer --------------------#
+	def __init__(self):
+		#-------------------- Head --------------------#		
+		self.__header = '''
+<!DOCTYPE>
+<html>
+	<head>
+		<title>Countries</title>
+		<link href='http://fonts.googleapis.com/css?family=Lato:300|Montserrat' rel='stylesheet' type='text/css'>
+		<link rel='stylesheet' href='css/c.css' />
+	</head>
+	<body>
+		<div id="container">'''
