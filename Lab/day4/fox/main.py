8c228ca80e8e96c6f4796c89a1d57bf73ff03d29
Russ93 <russ93@fullsail.edu>
2014-01-18 19:25:20 -0500

    finished the phylum

--- Lab/day4/fox/main.py
+++ Lab/day4/fox/main.py
@@ -7,6 +7,7 @@ from htm import Page
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
 		page = Page()
+
 		self.response.write(page.head())
 		if self.request.GET:
 			a = int(self.request.GET['animal'])
@@ -17,3 +18,25 @@ class MainHandler(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
 	('/', MainHandler)
 ], debug=True)
+class Chordata(object):
+	def __init__(self):
+		self._phy = 'Chordata'
+		self._say = ''
+	@property
+	def phy(self):
+		return self._phy
+	@property
+	def avg(self):
+		return self._avg
+	@property
+	def hab(self):
+		return self._hab
+	@property
+	def geo(self):
+		return self._geo
+	@property
+	def img(self):
+		return self._img
+	@property
+	def say(self):
+		return self._say
