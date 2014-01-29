f728830f0846ad124613483620c2d584eeb8af97
Russ93 <russ93@fullsail.edu>
2014-01-29 13:45:49 -0500

    instantiated the html and css

---
+++ Lab/day6/api/htm.py
@@ -0,0 +1,66 @@
+import urllib2
+import json
+
+#-------------------------------------------------- Views --------------------------------------------------#
+class Page():
+#-------------------- initializer --------------------#
+	def __init__(self):
+		#-------------------- Head --------------------#		
+		self.__header='''
+<!DOCTYPE>
+<html>
+	<head>
+		<title></title>
+		<link href='http://fonts.googleapis.com/css?family=Lato:300|Raleway' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="c/n.css">
+		<link rel="stylesheet" href="c/m.css">
+		<script type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.js"></script>
+		<script>$(document).ready(function(){$("#zip").keydown(function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,190])||65==e.keyCode&&e.ctrlKey===!0||e.keyCode>=35&&e.keyCode<=39||(e.shiftKey||(e.keyCode<48||e.keyCode>57)&&(e.keyCode<96||e.keyCode>105))&&e.preventDefault()})});</script>
+	</head>
+	<body>'''
+		#-------------------- Buttons --------------------#
+		self.__nav='''
+			<nav>
+				<img src='i/logo.png' />
+				<form>
+					<input type='text' name='zip' placeholder='Enter the zip code for local deals' id='zip' maxlength="5" required='required' />
+					<button>Submit</button>
+				</form>
+			</nav>'''
+		self.__cta='''
+	<div id='cta'>
+		<h1>Welcome to</h1>
+		<img src='i/logo.png' />
+		<p>You're one stop for a great combination of Living Social, Groupon, and all Local Stores. Just enter you're zip code below and get all of the great deals with a 20 miles radius of where you live.</p>
+		<form>
+			<input type="text" name="zip" placeholder="Enter the zip code for local deals" id='zip' maxlength="5" required='required' />
+			<button>Submit</button>
+		</form>
+	</div>
+		'''
+		#-------------------- Closers --------------------#
+		self.__foot='''
+	</body>
+</html>'''
+#-------------------- Returners --------------------#
+	@property
+	def head(self):
+		return self.__header
+	@property
+	def nav(self):
+		return self.__nav
+	@property
+	def cta(self):
+		return self.__cta
+	@property
+	def template(self):
+		return self.__tmp
+	@property
+	def foot(self):
+		return self.__foot
+	@property
+	def ul(self):
+		return self.__ul
+	@ul.setter
+	def ul(self, val):
+		self.__ul = val
--- Lab/day6/api/main.py
+++ Lab/day6/api/main.py
@@ -10,7 +10,18 @@ import json #library for working with xml
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
-		self.response.write('Hello World')
+		page = Page()
+
+		self.response.write(page.head)
+		if self.request.GET:
+			self.response.write(page.nav)
+			model = Model(self.request.GET['zip'])
+			do = model.do
+			page.tmp(do)
+			self.response.write(page.ul)
+		else:
+			self.response.write(page.cta)
+		self.response.write(page.foot)
 
 app = webapp2.WSGIApplication([
 	('/', MainHandler)
